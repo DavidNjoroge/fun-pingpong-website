2c0f8682423fc8208a2b29b4bbdc2e996e2f441e
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-08-18 18:46:53 +0300

    update the business logic to capture user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
-jQuery(document).ready(function($) {
-  $('.form-inline').submit(function(event) {
-    /* Act on the event */
-    event.preventDefault()
-    var numb = parseInt($('input#inputs').val());
-    console.log(numb);
-    countr()
-  });
-});
-
-
 var countr = function(numb) {
-  for (var i = 8; i <= 16; i++) {
+  for (var i = 1; i <= numb; i++) {
     if ((i % 3 === 0) && (i % 5 === 0)) {
       alert("ping pong");
+
     } else if (i % 5 === 0) {
 
       alert("pong");
     } else if (i % 3 === 0) {
 
+
       alert("ping");
     } else {
-
       alert(i);
     }
   }
 };
+
+
+
+jQuery(document).ready(function($) {
+  $('.form-inline').submit(function(event) {
+    /* Act on the event */
+    event.preventDefault()
+    var numb = parseInt($('input#inputs').val());
+    countr(numb)
+
+    $('.theList').text();
+  });
+});
